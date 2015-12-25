e59b98ed7a7098b82746c6d05282a2b0c74e17b3
srinidhi-shankar <manusrinidhi10@gmail.com>
2015-12-25 14:34:12 +0530

    added a comment.. Sample

--- app/src/main/java/com/streetsnax/srinidhi/streetsnax/login.java
+++ app/src/main/java/com/streetsnax/srinidhi/streetsnax/login.java
@@ -9,5 +9,7 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //just a comment
     }
 }
