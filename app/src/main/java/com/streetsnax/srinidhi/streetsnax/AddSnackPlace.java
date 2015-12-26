ead1c7149dafbb9a1b60118a4b2a3d85ff90ff1b
srinidhi-shankar <manusrinidhi10@gmail.com>
2015-12-26 17:34:54 +0530

    floating plus button and sample submit your snack activity

---
+++ app/src/main/java/com/streetsnax/srinidhi/streetsnax/AddSnackPlace.java
@@ -0,0 +1,15 @@
+package com.streetsnax.srinidhi.streetsnax;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddSnackPlace extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_snack);
+
+    }
+}
