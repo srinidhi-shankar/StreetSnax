de5abdbb597cb9042acc517a73b30e14891f3a62
Manjunath P R <mangzee@gmail.com>
2016-01-03 18:13:43 +0530

    added share link,dim the image in information page, added back button, added directions

--- app/src/main/java/com/streetsnax/srinidhi/streetsnax/SearchScrollingActivity.java
+++ app/src/main/java/com/streetsnax/srinidhi/streetsnax/SearchScrollingActivity.java
@@ -1,19 +1,25 @@
 package com.streetsnax.srinidhi.streetsnax;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -29,6 +35,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class SearchScrollingActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -78,6 +85,7 @@ public class SearchScrollingActivity extends AppCompatActivity implements OnMapR
         }
 
         Picasso.with(this).load(itemDetails.getItemImageSrc()).into(imageinfoview);
+        imageinfoview.setColorFilter(Color.rgb(123, 123, 123), android.graphics.PorterDuff.Mode.MULTIPLY);
         //new DownloadImageTask(imageinfoview).execute(itemDetails.getItemImageSrc());
         infotoolbar_layout.setTitle(itemDetails.getsnackPlaceName());
         MapTitle = itemDetails.getsnackPlaceName();
@@ -99,13 +107,28 @@ public class SearchScrollingActivity extends AppCompatActivity implements OnMapR
             @Override
             public void onClick(View view) {
                 //replace this with directions from current location
-                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + latitude + "," + longitude + "\""));
-                i.setClassName("com.google.android.apps.maps",
-                        "com.google.android.maps.MapsActivity");
-                startActivity(i);
+//                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + latitude + "," + longitude + "\""));
+//                i.setClassName("com.google.android.apps.maps",
+//                        "com.google.android.maps.MapsActivity");
+//                startActivity(i);
+
+                String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?daddr=%f,%f (%s)", latitude, longitude, infotoolbar_layout.getTitle());
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                try {
+                    startActivity(intent);
+                } catch (ActivityNotFoundException ex) {
+                    try {
+                        Intent unrestrictedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                        startActivity(unrestrictedIntent);
+                    } catch (ActivityNotFoundException innerEx) {
+                        Toast.makeText(SearchScrollingActivity.this, "Please install a maps application", Toast.LENGTH_LONG).show();
+                    }
+                }
             }
         });
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
 
@@ -139,6 +162,36 @@ public class SearchScrollingActivity extends AppCompatActivity implements OnMapR
         map.getUiSettings().setMyLocationButtonEnabled(true);
         map.getUiSettings().setZoomControlsEnabled(true);
         map.getUiSettings().setMapToolbarEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_scrolling, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_share) {
+            try {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_SUBJECT, "StreetSnax");
+                String url = "http://maps.google.com/maps?daddr=" + latitude + "," + longitude;
+                String sAux = "\nHey find this awesome streetfood " + infotoolbar_layout.getTitle() + "\n\n";
+                sAux = sAux + "address:\n\n" + txtViewAddress.getText() + "\n\n";
+                sAux += "Maps: " + url + "\n\nFor more details install StreetSnax";
+                i.putExtra(Intent.EXTRA_TEXT, sAux);
+                startActivity(Intent.createChooser(i, "choose one"));
+            } catch (Exception e) { //e.toString();
+            }
+            return true;
+        } else if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
